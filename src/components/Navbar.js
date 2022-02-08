b3e54c8a2200f4b3a22ed9f4eedc35e5edcc5538
TatsuKenshi <rockinbg@gmail.com>
2022-02-08 22:38:07 +0100

    basic work

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,169 @@
-import React from 'react';
-import styled from 'styled-components';
-import { useAuth0 } from '@auth0/auth0-react';
+import React from "react";
+import styled from "styled-components";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Navbar = () => {
   return <h2>navbar component</h2>;
 };
 
-const Wrapper = styled.nav`
-  padding: 1.5rem;
-  margin-bottom: 4rem;
-  background: var(--clr-white);
-  text-align: center;
-  display: grid;
-  grid-template-columns: auto auto 100px;
-  justify-content: center;
-  align-items: center;
-  gap: 1.5rem;
-  h4 {
-    margin-bottom: 0;
-    font-weight: 400;
-  }
-  img {
-    width: 35px !important;
-    height: 35px;
-    border-radius: 50%;
-    object-fit: cover;
-  }
-  button {
-    background: transparent;
-    border: transparent;
-    font-size: 1.2rem;
-    text-transform: capitalize;
-    letter-spacing: var(--spacing);
-    color: var(--clr-grey-5);
-    cursor: pointer;
-  }
-`;
+// const Wrapper = styled.article`
+//   background: var(--clr-white);
+//   padding: 1.5rem 2rem;
+//   border-top-right-radius: var(--radius);
+//   border-bottom-left-radius: var(--radius);
+//   border-bottom-right-radius: var(--radius);
+//   position: relative;
+//   &::before {
+//     content: 'user';
+//     position: absolute;
+//     top: 0;
+//     left: 0;
+//     transform: translateY(-100%);
+//     background: var(--clr-white);
+//     color: var(--clr-grey-5);
+//     border-top-right-radius: var(--radius);
+//     border-top-left-radius: var(--radius);
+//     text-transform: capitalize;
+//     padding: 0.5rem 1rem 0 1rem;
+//     letter-spacing: var(--spacing);
+//     font-size: 1rem;
+//   }
+//   header {
+//     display: grid;
+//     grid-template-columns: auto 1fr auto;
+//     align-items: center;
+//     column-gap: 1rem;
+//     margin-bottom: 1rem;
+//     img {
+//       width: 75px;
+//       height: 75px;
+//       border-radius: 50%;
+//     }
+//     h4 {
+//       margin-bottom: 0.25rem;
+//     }
+//     p {
+//       margin-bottom: 0;
+//     }
+//     a {
+//       color: var(--clr-primary-5);
+//       border: 1px solid var(--clr-primary-5);
+//       padding: 0.25rem 0.75rem;
+//       border-radius: 1rem;
+//       text-transform: capitalize;
+//       letter-spacing: var(--spacing);
+//       transition: var(--transition);
+//       cursor: pointer;
+//       &:hover {
+//         background: var(--clr-primary-5);
+//         color: var(--clr-white);
+//       }
+//     }
+//   }
+//   .bio {
+//     color: var(--clr-grey-3);
+//   }
+//   .links {
+//     p,
+//     a {
+//       margin-bottom: 0.25rem;
+//       display: flex;
+//       align-items: center;
+//       svg {
+//         margin-right: 0.5rem;
+//         font-size: 1.3rem;
+//       }
+//     }
+//     a {
+//       color: var(--clr-primary-5);
+//       transition: var(--transition);
+//       svg {
+//         color: var(--clr-grey-5);
+//       }
+//       &:hover {
+//         color: var(--clr-primary-3);
+//       }
+//     }
+//   }
+// `;const Wrapper = styled.article`
+//   background: var(--clr-white);
+//   padding: 1.5rem 2rem;
+//   border-top-right-radius: var(--radius);
+//   border-bottom-left-radius: var(--radius);
+//   border-bottom-right-radius: var(--radius);
+//   position: relative;
+//   &::before {
+//     content: 'user';
+//     position: absolute;
+//     top: 0;
+//     left: 0;
+//     transform: translateY(-100%);
+//     background: var(--clr-white);
+//     color: var(--clr-grey-5);
+//     border-top-right-radius: var(--radius);
+//     border-top-left-radius: var(--radius);
+//     text-transform: capitalize;
+//     padding: 0.5rem 1rem 0 1rem;
+//     letter-spacing: var(--spacing);
+//     font-size: 1rem;
+//   }
+//   header {
+//     display: grid;
+//     grid-template-columns: auto 1fr auto;
+//     align-items: center;
+//     column-gap: 1rem;
+//     margin-bottom: 1rem;
+//     img {
+//       width: 75px;
+//       height: 75px;
+//       border-radius: 50%;
+//     }
+//     h4 {
+//       margin-bottom: 0.25rem;
+//     }
+//     p {
+//       margin-bottom: 0;
+//     }
+//     a {
+//       color: var(--clr-primary-5);
+//       border: 1px solid var(--clr-primary-5);
+//       padding: 0.25rem 0.75rem;
+//       border-radius: 1rem;
+//       text-transform: capitalize;
+//       letter-spacing: var(--spacing);
+//       transition: var(--transition);
+//       cursor: pointer;
+//       &:hover {
+//         background: var(--clr-primary-5);
+//         color: var(--clr-white);
+//       }
+//     }
+//   }
+//   .bio {
+//     color: var(--clr-grey-3);
+//   }
+//   .links {
+//     p,
+//     a {
+//       margin-bottom: 0.25rem;
+//       display: flex;
+//       align-items: center;
+//       svg {
+//         margin-right: 0.5rem;
+//         font-size: 1.3rem;
+//       }
+//     }
+//     a {
+//       color: var(--clr-primary-5);
+//       transition: var(--transition);
+//       svg {
+//         color: var(--clr-grey-5);
+//       }
+//       &:hover {
+//         color: var(--clr-primary-3);
+//       }
+//     }
+//   }
+// `;
 
 export default Navbar;
