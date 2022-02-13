498532c03f59f10d2fb41d2be030a029d7a57e52
TatsuKenshi <rockinbg@gmail.com>
2022-02-13 20:12:02 +0100

    css

--- src/components/User.js
+++ src/components/User.js
@@ -6,14 +6,14 @@ const User = () => {
   return <h2>search component</h2>;
 };
 
-// const Wrapper = styled.div`
-//   padding-top: 2rem;
-//   display: grid;
-//   gap: 3rem 2rem;
-//   @media (min-width: 992px) {
-//     grid-template-columns: 1fr 1fr;
-//   }
-/* align-items: start; */
-// `;
+const Wrapper = styled.div`
+  padding-top: 2rem;
+  display: grid;
+  gap: 3rem 2rem;
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr;
+  }
+  /* align-items: start; */
+`;
 
 export default User;
--- src/pages/AuthWrapper.js
+++ src/pages/AuthWrapper.js
@@ -6,13 +6,13 @@ function AuthWrapper() {
   return <h2>authwrapper component</h2>;
 }
 
-// const Wrapper = styled.section`
-//   min-height: 100vh;
-//   display: grid;
-//   place-items: center;
-//   img {
-//     width: 150px;
-//   }
-// `;
+const Wrapper = styled.section`
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+  img {
+    width: 150px;
+  }
+`;
 
 export default AuthWrapper;
